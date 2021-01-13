09ef18ab613c5fd8b69ffbad5ca9000efb5ad4d6
Sherif elmotayam <sherifelmotayam@gmail.com>
2021-01-13 20:34:31 +0200

    home page is done with all features

--- fbreg.php
+++ fbreg.php
@@ -1,5 +1,8 @@
+<?php
+session_start();
+?>
 <html>
-    
+
     <head>
     
         <link rel="stylesheet" href="fb.css">
@@ -18,7 +21,7 @@
                   
                   
                   <input type="text" name="password" placeholder="Pass">
-                <input type="submit" name="Login" placeholder="Login">
+                <input type="submit" name="submit_Login" placeholder="Login">
               </form>
               </div>
               </div>
@@ -28,8 +31,9 @@
         <div class="mydiv">
         <h1> Create a new account</h1>
         <p >its quick a easy.</p>
-            <form action="fbreg.php" method="post">
-        
+            <form action="fbreg.php" method="post" enctype="multipart/form-data">
+        <input type="file" name =image placeholder="profile picture">
+                <br>
             <input type="text" name=Fname placeholder="First name">
             <input type="text" name=Lname placeholder="Surname">
             <br>
@@ -45,7 +49,7 @@
             <input type="radio" name=Gender value="Female">Female<br>
             
             <br><br>
-            <input type="submit" name=submit value="Sign Up">
+                <input type="submit" name=submit_reg value="Sign Up">
         </form>
         </div>
         
@@ -55,32 +59,53 @@
         $hostname="localhost";
         $password="";
         $dbname="facebook";
-        
-        
-    if($_SERVER["REQUEST_METHOD"]=="POST")
-    {
-        
-        $conn=mysqli_connect($hostname,$username,$password,$dbname);
+    
+         $conn=mysqli_connect($hostname,$username,$password,$dbname);
         if(!$conn)
         {
             
             die("filed to connect to database ".mysqli_connect_error());
-        }
+               }
         else
         {
             echo "Connected sucessfully to database";
-         }
+        }
+
+        
+    if(isset($_POST['submit_Login']))
+      {
+          $_SESSION["Email"]=$_POST['Email'];
+        $_SESSION["pass"]=$_POST['password'];
+        
+        $getdata="SELECT * FROM `fbreg` WHERE Email='".$_POST['Email']."' and password='".$_POST['password']."'";
+        
+          if(mysqli_query($conn,$getdata))
+       {
     
+              
+              header('location:homepage.php');
+              
+       }
+        else
+        {
+            
+            echo"login failed".mysqli_connect_error(); 
+        }
         
         
- $FName=$_POST['Fname'];
+    }
+    if(isset($_POST['submit_reg']))
+    {
+        
+           
+        
+        
+        $FName=$_POST['Fname'];
         $LName=$_POST['Lname'];
         $Email=$_POST['Email'];
         $password=$_POST['password'];
         $Date=$_POST['Date-of-birth'];
         $Gender=$_POST['Gender'];
-
-        
         
     echo "First name: ".$FName."<br>";
     echo "Last name: ".$LName."<br>";
@@ -90,26 +115,31 @@
     echo "Gender: ".$Gender."<br>";    
        
         
-        
-        
-        
- $insertdata="INSERT INTO `fbreg`( `Fname`, `Lname`, `Email`, `password`, `Gender`, `Dateofbirth`) VALUES ('$FName','$LName','$Email','$password','$Gender','$Date')
-    ";
     
-    
-    if(mysqli_query($conn,$insertdata))
-    {
+         $filename=$_FILES['image']['name'];
+        $tmp=$_FILES['image']['tmp_name'];
+        move_uploaded_file($tmp,"uploadpic/".$filename);
+        echo "file name: ".$filename."<br>";
+         echo"File location before move".$tmp."<br>";
+               $insertdata="INSERT INTO `fbreg`(`Fname`, `Lname`, `Email`, `password`, `Gender`, `Dateofbirth`,`Image`) VALUES ('$FName','$LName','$Email','$password','$Gender','$Date','$filename')";
+
+     if(mysqli_query($conn,$insertdata))
+       {
         echo "Table created successfully";
         
-        
-    }
+       }
         else
         {
             
             echo"Table inserted failed".mysqli_connect_error(); 
         }
+     }
     
-    }
+    
+        
+    
+    
+    
     
          ?>
              
---
+++ homepage.php
@@ -0,0 +1,69 @@
+<?php
+session_start();   
+?>
+<html>
+    <head>
+
+    <link rel="stylesheet" href="hmpge.css">
+    </head>
+    <body>
+<div >
+        <ul>
+      
+                  <div class="topnav">
+
+    <?php
+          $username="root";
+        $hostname="localhost";
+        $password="";
+        $dbname="facebook";
+            
+               $conn=mysqli_connect($hostname,$username,$password,$dbname);
+ 
+     $get_info="SELECT * FROM `fbreg` WHERE Email='".$_SESSION["Email"]."'";   
+    $result=mysqli_query($conn,$get_info);
+        if($result)
+     {
+        
+         while($row=mysqli_fetch_row($result))
+    {   
+            $FN=$row[0];
+            $LN=$row[1];
+            $GN=$row[4];
+            $DB=$row[5];
+            $IMG=$row[6];
+     print "<img src = 'uploadpic/$IMG' style = 'border-radius: 50%; width: 40px; height: 40px; margin-left: 85px; padding-top: 0px; margin-buttom:-80px; margin-left:30px;'>";  
+     
+
+     
+         }
+                 
+
+   
+        }
+              
+              
+              
+    ?>
+         
+         <a  href="fbreg.php">log out</a>
+               <a  href="#">view profile</a>
+           <input type="text" placeholder="Search..">
+
+
+               <a href="#" style=" margin-right:530px;">Home page </a>
+           
+
+            
+        
+                          </div>
+
+               
+        </ul>
+
+             
+ 
+        </div>
+    </body>
+    
+</html>
