64320f7cdf2eea17acc95def9ea2ed2016292181
Sherif elmotayam <sherifelmotayam@gmail.com>
2021-01-13 21:57:31 +0200

    home page is dont

--- homepage.php
+++ homepage.php
@@ -64,6 +64,21 @@ session_start();
              
  
         </div>
+        
+        
+        <div class="bar">
+        
+        <ul>
+ <br>
+            <li><a  href="#home">Home</a></li>
+  <br>
+            <li><a href="#news">News</a></li>
+ <br>
+            <li><a href="#contact">Contact</a></li>
+ <br>
+            <li><a href="#about">About</a></li>
+</ul>
+        </div>
     </body>
     
 </html>
